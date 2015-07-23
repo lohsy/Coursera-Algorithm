6e7d38929da45c43665088220db0bc807b47a134
Michael Loh <lohsy@bii.a-star.edu.sg>
2015-07-23 10:13:15 +0800

    Modified style of program

--- Assignment1/src/Percolation.java
+++ Assignment1/src/Percolation.java
@@ -8,30 +8,30 @@ public class Percolation {
 	// create N-by-N grid, with all sites blocked
 	public Percolation(int N) {
 		this.N = N;
-		if (N<0) 
+		if (N < 0) 
 			throw new IllegalArgumentException("N is less than 0");
 		grid = new boolean [N][N];
 		uf = new WeightedQuickUnionUF(N*N);
 	}
 
-	// open site (row i, column j) if it is not open already
+	// open site (row i, column j) if  it is not open already
 	public void open(int i, int j) {
-		if(i<1 || i > N || j <1 || j > N)
+		if (i < 1 || i > N || j  < 1 || j > N)
 			throw new IndexOutOfBoundsException("i: " + i + " j: " + j);
 		
-		if(!isOpen(i, j)){
+		if (!isOpen(i, j)) {
 			grid[i-1][j-1] = true;
-			if(i>1)
-				if(grid[i-1][j-1] & grid[i-2][j-1])
+			if (i > 1)
+				if (grid[i-1][j-1] & grid[i-2][j-1])
 					uf.union(coordsToIndex(i, j),coordsToIndex(i-1, j));
-			if(i<N)
-				if(grid[i-1][j-1] & grid[i][j-1])
+			if (i < N)
+				if (grid[i-1][j-1] & grid[i][j-1])
 					uf.union(coordsToIndex(i, j),coordsToIndex(i+1, j));
-			if(j>1)
-				if(grid[i-1][j-1] & grid[i-1][j-2])
+			if (j > 1)
+				if (grid[i-1][j-1] & grid[i-1][j-2])
 					uf.union(coordsToIndex(i, j),coordsToIndex(i, j-1));
-			if(j<N)
-				if(grid[i-1][j-1] & grid[i-1][j])
+			if (j < N)
+				if (grid[i-1][j-1] & grid[i-1][j])
 					uf.union(coordsToIndex(i, j),coordsToIndex(i, j+1));
 
 		}
@@ -39,22 +39,22 @@ public class Percolation {
 
 	// is site (row i, column j) open?
 	public boolean isOpen(int i, int j) {
-		if(i<1 || i > N || j <1 || j > N)
+		if (i < 1 || i > N || j  < 1 || j > N)
 			throw new IndexOutOfBoundsException("i: " + i + " j: " + j);
 		return grid[i-1][j-1];
 	}
 
 	// is site (row i, column j) full?
 	public boolean isFull(int i, int j) {
-		if(i<1 || i > N || j <1 || j > N)
+		if (i < 1 || i > N || j  < 1 || j > N)
 			throw new IndexOutOfBoundsException("i: " + i + " j: " + j);
 		
 		boolean full = false;
-		if(grid[i-1][j-1]){
-			for(int index=1;index<=N;index++){
+		if (grid[i-1][j-1]){
+			for(int index = 1; index <= N; index++) {
 				full = uf.connected(coordsToIndex(1, index),
 						coordsToIndex(i, j));
-				if(full)
+				if (full)
 					break;
 			}
 		}
@@ -64,9 +64,9 @@ public class Percolation {
 	// does the system percolate?
 	public boolean percolates() {
 		boolean percolates = false;
-		for(int index=1;index<=N;index++){
+		for (int index = 1;index <= N; index++) {
 			percolates = isFull(N, index);
-			if(percolates)
+			if (percolates)
 				break;
 		}
 		return percolates;
--- Assignment1/src/PercolationStats.java
+++ Assignment1/src/PercolationStats.java
@@ -1,24 +1,24 @@
 public class PercolationStats {
 
-	private double thresholds [];
+	private double [] thresholds;
 	
 	// perform T independent experiments on an N-by-N grid
 	public PercolationStats(int N, int T) {
 		thresholds = new double [T];
 		
-		for(int e=1; e<=T; e++){
+		for(int e=1; e <= T; e++) {
 			//StdOut.println("Experiment " + e);
 			Percolation p = new Percolation(N);
 			int count=0;
-			while(true){
+			while(true) {
 				int i = StdRandom.uniform(N) + 1;
 				int j = StdRandom.uniform(N) + 1;
-				if(!p.isOpen(i, j)){
+				if (!p.isOpen(i, j)) {
 					p.open(i, j);
 					count++;
-					if(p.percolates()){
+					if (p.percolates()) {
 						//StdOut.println((double)count/((double)N*N));
-						thresholds[e-1] = (double)count/((double)N*N);
+						thresholds[e-1] = (double) count/( (double) N*N);
 						break;
 					}
 				}
@@ -45,7 +45,7 @@ public class PercolationStats {
 	public double confidenceLo() {
 		double mean = mean();
 		double sd = stddev();
-		return mean - (sd*1.96/Math.sqrt(thresholds.length));
+		return mean - (sd * 1.96 / Math.sqrt(thresholds.length));
 	}
 
 	// high endpoint of 95% confidence interval
