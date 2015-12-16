60537e78e339450ff024e3db93192a8080427f95
Michael Loh <lohsy@bii.a-star.edu.sg>
2015-12-16 17:00:28 +0800

    Fixed imports and indentation

--- Assignment10/src/BurrowsWheeler.java
+++ Assignment10/src/BurrowsWheeler.java
@@ -3,93 +3,91 @@ import java.util.Comparator;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
-
 
 public class BurrowsWheeler {
     // apply Burrows-Wheeler encoding, reading from standard input and writing to standard output
     public static void encode() {
     	
-    	String message = StdIn.readLine();
-    	CircularSuffixArray csa = new CircularSuffixArray(message);
-    	
-    	StringBuilder sb = new StringBuilder();
-    	int first = 0;
-    	
-    	for(int i=0;i<message.length();i++) {
-    		int charPosition = (csa.index(i) + message.length() - 1) % message.length();
-    		sb.append(message.charAt(charPosition));
-    		if(csa.index(i) == 0)
-    			first = i;
-    	}
-    	
-    	BinaryStdOut.write(first);
-    	BinaryStdOut.write(sb.toString(), 8);
-    	BinaryStdOut.close();
+		String message = BinaryStdIn.readString();
+		CircularSuffixArray csa = new CircularSuffixArray(message);
+
+		StringBuilder sb = new StringBuilder();
+		int first = 0;
+
+		for (int i = 0; i < message.length(); i++) {
+			int charPosition = (csa.index(i) + message.length() - 1)
+					% message.length();
+			sb.append(message.charAt(charPosition));
+			if (csa.index(i) == 0)
+				first = i;
+		}
+
+		BinaryStdOut.write(first);
+		BinaryStdOut.write(sb.toString(), 8);
+		BinaryStdOut.close();
     }
 
     // apply Burrows-Wheeler decoding, reading from standard input and writing to standard output
     public static void decode() {
     	
-    	int first = BinaryStdIn.readInt();
-    	String t = BinaryStdIn.readString();
-    	Character [] sorted = new Character [t.length()];
-    	for(int i=0;i<t.length();i++)
-    		sorted[i] = t.charAt(i);
+		int first = BinaryStdIn.readInt();
+		String t = BinaryStdIn.readString();
+		Character[] sorted = new Character[t.length()];
+		for (int i = 0; i < t.length(); i++)
+			sorted[i] = t.charAt(i);
 
-    	//sort the chars
-    	Arrays.sort(sorted, new Comparator<Character>() {
+		// sort the chars
+		Arrays.sort(sorted, new Comparator<Character>() {
 
 			@Override
 			public int compare(Character c1, Character c2) {
-				if(c1 < c2)
-					return - 1;
+				if (c1 < c2)
+					return -1;
 				else if (c1 > c2)
 					return 1;
-				else 
+				else
 					return 0;
 			}
-		}); 
-    	
-    	//obtain next array
-    	int [] next = new int [t.length()];
-    	for(int i=0;i<next.length;i++)
-    		next[i] =-1;
-    	
-    	int count=0;
-    	for(int i=0;i<sorted.length;i++){
-    		
-    		if(i>0 && sorted[i] == sorted[i-1]) 
-    			count++;
-    		else
-    			count = 0;
-    		
-    		int countdown = count;
-    		for(int j=0;j<t.length();j++) {
-    			
-    			if(sorted[i] == t.charAt(j)) {
-    				if(countdown == 0) {
-    					next[i] = j;
-    					break;
-    				} else
-    					countdown --;
-    			}
-    		}
-    	}
-    	
-    	//build message
-    	StringBuilder sb = new StringBuilder();
-    	
-    	int index = first;
-    	while(next[index] != first) {
-    		sb.append(sorted[index]);
-    		index = next[index];
-    	}
-    	sb.append(sorted[index]);
-    	
-    	BinaryStdOut.write(sb.toString(), 8);
-    	BinaryStdOut.close();
+		});
+
+		// obtain next array
+		int[] next = new int[t.length()];
+		for (int i = 0; i < next.length; i++)
+			next[i] = -1;
+
+		int count = 0;
+		for (int i = 0; i < sorted.length; i++) {
+
+			if (i > 0 && sorted[i] == sorted[i - 1])
+				count++;
+			else
+				count = 0;
+
+			int countdown = count;
+			for (int j = 0; j < t.length(); j++) {
+
+				if (sorted[i] == t.charAt(j)) {
+					if (countdown == 0) {
+						next[i] = j;
+						break;
+					} else
+						countdown--;
+				}
+			}
+		}
+
+		// build message
+		StringBuilder sb = new StringBuilder();
+
+		int index = first;
+		while (next[index] != first) {
+			sb.append(sorted[index]);
+			index = next[index];
+		}
+		sb.append(sorted[index]);
+
+		BinaryStdOut.write(sb.toString(), 8);
+		BinaryStdOut.close();
     	
 //    	System.out.println("first: " + first);
 //    	System.out.println("encoded: " + t);
@@ -108,16 +106,16 @@ public class BurrowsWheeler {
 //    	System.out.println(sb.toString());
     }
 
-    // if args[0] is '-', apply Burrows-Wheeler encoding
-    // if args[0] is '+', apply Burrows-Wheeler decoding
-    public static void main(String[] args) {
-    	if(args[0].charAt(0) == '-')
-    		BurrowsWheeler.encode();
-		else if(args[0].charAt(0) == '+')
+	// if args[0] is '-', apply Burrows-Wheeler encoding
+	// if args[0] is '+', apply Burrows-Wheeler decoding
+	public static void main(String[] args) {
+		if (args[0].charAt(0) == '-')
+			BurrowsWheeler.encode();
+		else if (args[0].charAt(0) == '+')
 			BurrowsWheeler.decode();
-		else{
-			StdOut.println("Invalid argument");
+		else {
+			BinaryStdOut.write("Invalid argument");
 		}
-    }
+	}
 }
 
--- Assignment10/src/CircularSuffixArray.java
+++ Assignment10/src/CircularSuffixArray.java
@@ -7,9 +7,8 @@ public class CircularSuffixArray {
 	
 	// circular suffix array of s
 	public CircularSuffixArray(String s) {
-		if(s == null)
+		if (s == null)
 			throw new NullPointerException();
-		
 		generateSuffixArray(s);
 	}
 	
@@ -20,29 +19,31 @@ public class CircularSuffixArray {
 
 	// returns index of ith sorted suffix
 	public int index(int i) {
-		if(i<0 || i>=length())
+		if (i < 0 || i >= length())
 			throw new IndexOutOfBoundsException();
 		return indexes[i];
 	}
 
 	private void generateSuffixArray(final String s) {
 		
-		indexes = new Integer [s.length()];
-		for(int i=0;i<indexes.length;i++)
+		indexes = new Integer[s.length()];
+		for (int i = 0; i < indexes.length; i++)
 			indexes[i] = i;
-		
+
 		Arrays.sort(indexes, new Comparator<Integer>() {
 
 			@Override
 			public int compare(Integer i1, Integer i2) {
-				
-				for(int i=0;i<s.length();i++) {
-					if(s.charAt((i1+i)%length()) < s.charAt((i2+i)%length()))
+
+				for (int i = 0; i < s.length(); i++) {
+					if (s.charAt((i1 + i) % length()) < s.charAt((i2 + i)
+							% length()))
 						return -1;
-					if(s.charAt((i1+i)%length()) > s.charAt((i2+i)%length()))
+					if (s.charAt((i1 + i) % length()) > s.charAt((i2 + i)
+							% length()))
 						return 1;
 				}
-				
+
 				return 0;
 			}
 		});
