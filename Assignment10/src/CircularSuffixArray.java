9dd019ba54e8c94f29401b3d8aee05f58ae69f73
Michael Loh <lohsy@bii.a-star.edu.sg>
2015-12-16 17:30:23 +0800

    Changed CircularSuffixArray datastructure

--- Assignment10/src/CircularSuffixArray.java
+++ Assignment10/src/CircularSuffixArray.java
@@ -1,56 +1,28 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import edu.princeton.cs.algs4.SuffixArrayX;
 
 public class CircularSuffixArray {
 
-	private Integer [] indexes;
+	private SuffixArrayX sa;
 	
 	// circular suffix array of s
 	public CircularSuffixArray(String s) {
 		if (s == null)
 			throw new NullPointerException();
-		generateSuffixArray(s);
+		
+		sa = new SuffixArrayX(s);
+	
 	}
 	
 	// length of s
 	public int length() {
-		return indexes.length;
+		return sa.length();
 	}
 
 	// returns index of ith sorted suffix
 	public int index(int i) {
 		if (i < 0 || i >= length())
 			throw new IndexOutOfBoundsException();
-		return indexes[i];
-	}
-
-	private void generateSuffixArray(final String s) {
-		
-		indexes = new Integer[s.length()];
-		for (int i = 0; i < indexes.length; i++)
-			indexes[i] = i;
-
-		Arrays.sort(indexes, new Comparator<Integer>() {
-
-			@Override
-			public int compare(Integer i1, Integer i2) {
-
-				for (int i = 0; i < s.length(); i++) {
-					if (s.charAt((i1 + i) % length()) < s.charAt((i2 + i)
-							% length()))
-						return -1;
-					if (s.charAt((i1 + i) % length()) > s.charAt((i2 + i)
-							% length()))
-						return 1;
-				}
-
-				return 0;
-			}
-		});
-		
-		//for(int i : indexes) {
-		//	System.out.println(s.charAt(i) + " " + i);
-		//}
+		return sa.index(i);
 	}
 
 	// unit testing of the methods (optional)
